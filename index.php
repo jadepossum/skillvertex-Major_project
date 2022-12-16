d5e0a90713eb6fd50ebf682b65bd5de3cfc2278c
jadepossum <105377140+jadepossum@users.noreply.github.com>
2022-12-16 21:59:55 +0530

    first episode

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>healthcare firm</title>
+    <style>
+        *{
+            margin:0px;
+            padding:0px;
+        }
+        body{
+            
+        }
+        .hospitals{
+            color:lightblue;
+            background-color:lightcoral;
+            position:relative;
+            width:100%;
+            height:500px;
+            font-size:3rem;
+            display:flex;
+        }
+        .hospitals > img{
+            width:400px;
+            background-color:blue;
+        }
+    </style>
+</head>
+<body>
+    <div class="hospitals">
+        <a href="#">our hospitals</a>
+        <img src="./img/hospital.png" width="400px" alt="image not found">
+    </div>
+    <div class="instantQuotes"></div>
+    <div class="reports">
+        <img src="./img/reports.png" width="400px" alt="not found">
+    </div>
+    <div class="payments"></div>
+</body>
+</html>
